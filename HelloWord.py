14de2b0b6849faba697baebcb1b061b7ac0abc4f
Luis Orlando Cabrera Aldui <ealdui@MBP-ealdui.local>
2020-08-20 17:45:17 -0500

    resto de archivos

---
+++ HelloWord.py
@@ -0,0 +1,22 @@
+
+#esto es comentario
+#creado el dia 080520 - dia 54 de la 40tena
+
+print("Hello Word")
+print("mi nuevo - hello word")
+
+a = 1
+b = 2
+cal_suma = a+b
+cal_resta = a-b
+cal_mult = a*b
+cal_div = b//a
+
+print(cal_suma)
+print(cal_resta)
+print(cal_mult)
+print(cal_div)
+print(type(cal_mult))
+
+print(type(cal_div))
+
